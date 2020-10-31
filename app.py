42a84f71244bc2d57b482489469ca9bada1406d5
Catherine Meza <cat@Catherines-MacBook-Pro.local>
2020-10-31 13:37:43 -0600

    Created a test reddit bot

---
+++ app.py
@@ -0,0 +1,27 @@
+import praw
+import time
+
+from secret import client_id, client_secret, password
+
+reddit = praw.Reddit(
+    client_id=client_id,
+    client_secret=client_secret,
+    user_agent="<console:testbott:1.0>",
+    username="test-bott",
+    password=password
+)
+
+for submission in reddit.subreddit("learnpython").hot(limit=10):
+    # print("************")
+    # print(submission.title)
+
+# submission = reddit.submission(id="ji4e4e")
+
+    submission.comments.replace_more(limit=0)
+    for top_level_comment in submission.comments:
+        comment_lower = top_level_comment.body.lower()
+        if " bot " in comment_lower:
+            print("--------------")
+            print(top_level_comment.body)
+            # top_level_comment.reply("Did someone say my name? Oh, wait...sorry. My creator is still learning - don't be mad! :)")
+            # time.sleep(660)
